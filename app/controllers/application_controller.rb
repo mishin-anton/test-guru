67005e3a69538f28be37bbbce8e6f6f979a7f602
Anton Mishin <coal.msk@gmail.com>
2020-05-31 16:02:48 +0300

    lesson12-1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+    # I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+    I18n.locale != I18n.default_locale ? { lang: I18n.locale } : {}
   end
 
   def after_sign_in_path_for(resource)

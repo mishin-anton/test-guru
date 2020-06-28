d16863dcf5914b234e0290fa35bf859110b9f075
Anton Mishin <coal.msk@gmail.com>
2020-06-28 15:45:07 +0300

    Lesson17-7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    # params.require(:post).permit(:name, :email, :message)
-    params.fetch(:post, {}).permit(:name, :email, :message)
+    params.require(:post).permit(:name, :email, :message)
+    # params.fetch(:post, {}).permit(:name, :email, :message)
   end
 end

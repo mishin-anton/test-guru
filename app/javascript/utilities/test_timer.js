20e4f4eb332ff076a5718f249b3351c60f42b0bb
Anton Mishin <coal.msk@gmail.com>
2020-07-28 19:33:56 +0300

    lesson18-timer_1

--- app/javascript/utilities/test_timer.js
+++ app/javascript/utilities/test_timer.js
@@ -1,29 +1,30 @@
-document.addEventListener('turbolinks:load', function() {
-  var timer = document.getElementById('timer');
-  var duration = timer.dataset.duration;
+function setTimer() {
+  document.addEventListener('turbolinks:load', function() {
+    var timer = document.getElementById('timer');
+    var duration = timer.dataset.duration;
 
-  if (duration) {
-    var initialDateTime = new Date().getTime() + duration * 60 * 1000
+    if (duration) {
+      var initialDateTime = new Date().getTime() + duration * 60 * 1000
 
-    var t = setInterval(function() {
-      var dateTimeNow = new Date().getTime();
-      var distance = initialDateTime - dateTimeNow
+      var t = setInterval(function() {
+        var dateTimeNow = new Date().getTime();
+        var distance = initialDateTime - dateTimeNow
 
-      var hours   = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+        var hours   = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-      document.getElementById("timer").innerHTML = hours + "h " + minutes + "m " + seconds + "s "
+        document.getElementById("timer").innerHTML = hours + "h " + minutes + "m " + seconds + "s "
 
-      if (distance < 1000) {
-        clearInterval(t);
-        alert('Время истекло.')
-        finishTest()
-      }
-    }, 1000);
-  }
-
-})
+        if (distance < 1000) {
+          clearInterval(t);
+          alert('Время истекло.')
+          finishTest()
+        }
+      }, 1000);
+    }
+  })
+}
 
 function finishTest() {
   var host = window.location.origin
